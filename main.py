5657b2adcc71bf22016fee1f1e6b78b9c05488d3
DR <94643493+DanilRodenko@users.noreply.github.com>
2023-05-04 19:59:26 +0100

    Show station table on a home page

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
+stations = pd.read_csv('data_small/stations.txt', skiprows=17, index_col='STAID' )
+stations = stations[['STANAME                                 ']]
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 
@@ -19,4 +21,4 @@ def about(station, date):
             "temperatures": str(temperature)}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
